00dd6c03f1bcfef2473ecd980d70786b5cd06bac
Mariola Juchnikowska <mariola.oktaba@gmail.com>
2020-05-30 13:12:34 +0200

    add log on page and css styles

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector('.menu--js');
-menu.addEventListener('click', () => {
-    const nav = document.querySelector('.navigation--js')
-    nav.classList.toggle('navigation--open');
-})
+const menu = document.querySelector(".menu--js");
+menu.addEventListener("click", () => {
+    const nav = document.querySelector(".navigation--js");
+    nav.classList.toggle("navigation--open");
+});
